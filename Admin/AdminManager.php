cc7f332bf8133b46fb76ecb3a283ecd63c0e593d
hmbashar <hmbashar@gmail.com>
2025-07-03 14:16:36 +0600

    docs: Add documentation to Core and AdminManager classes

--- Admin/AdminManager.php
+++ Admin/AdminManager.php
@@ -1,58 +1,110 @@
 <?php
-namespace RevixReviews\Admin;
+/**
+ * Admin Manager
+ *
+ * This file is responsible for managing all the admin-side functionality of the plugin.
+ *
+ * @package RevixReviews
+ * @subpackage Admin
+ * @since 1.0.0
+ */
 
+namespace RevixReviews\Admin;
 
 // Exit if accessed directly.
 if (!defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
 use RevixReviews\Admin\Assets\Assets;
-use RevixReviews\Admin\Inc\Reviews\PostTypes\Reviews;
-use RevixReviews\Admin\Inc\Reviews\MetaBox\ReviewsMetaBox;
-
-use RevixReviews\Admin\Inc\Dashboard\Settings\Settings;
 use RevixReviews\Admin\Core\Core;
+use RevixReviews\Admin\Inc\Dashboard\Settings\Settings;
+use RevixReviews\Admin\Inc\Reviews\MetaBox\ReviewsMetaBox;
+use RevixReviews\Admin\Inc\Reviews\PostTypes\Reviews;
 
+/**
+ * Class AdminManager
+ *
+ * @package RevixReviews\Admin
+ * @since 1.0.0
+ */
 class AdminManager
 {
+	/**
+	 * The instance of the Assets class.
+	 *
+	 * @var Assets
+	 * @since 1.0.0
+	 */
+	protected $assets;
 
-    protected $assets;
-    protected $reviews;
-    protected $reviews_meta_box;
-   
+	/**
+	 * The instance of the Reviews class.
+	 *
+	 * @var Reviews
+	 * @since 1.0.0
+	 */
+	protected $reviews;
 
-    protected $settings;
-    protected $trustpilot_settings;
+	/**
+	 * The instance of the ReviewsMetaBox class.
+	 *
+	 * @var ReviewsMetaBox
+	 * @since 1.0.0
+	 */
+	protected $reviews_meta_box;
 
-    protected $core;
+	/**
+	 * The instance of the Settings class.
+	 *
+	 * @var Settings
+	 * @since 1.0.0
+	 */
+	protected $settings;
 
-    /**
-     * 
-     * Initialize the class and set its properties.
-     */
-    public function __construct()
-    {
-        $this->setConstants();
-        $this->init_admin_classes();
-    }
-    public function setConstants()
-    {
-        define('REVIXREVIEWS_ADMIN_ASSETS', plugin_dir_url(__FILE__) . 'Assets');
-        define('REVIXREVIEWS_ADMIN_PATH', plugin_dir_path(__FILE__));
+	/**
+	 * The instance of the Core class.
+	 *
+	 * @var Core
+	 * @since 1.0.0
+	 */
+	protected $core;
 
-    }
-    /**
-     * Initialize all admin classes
-     *
-     * @return void
-     */
-    private function init_admin_classes()
-    {
-        $this->assets = new Assets();
-        $this->reviews = new Reviews();
-        $this->reviews_meta_box = new ReviewsMetaBox();       
-        $this->settings = new Settings();
-        $this->core = new Core();
-    }
+	/**
+	 * AdminManager constructor.
+	 *
+	 * Initializes the admin-side of the plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct()
+	{
+		$this->set_constants();
+		$this->init_admin_classes();
+	}
+
+	/**
+	 * Sets the constants for the admin area.
+	 *
+	 * @since 1.0.0
+	 */
+	public function set_constants()
+	{
+		define('REVIXREVIEWS_ADMIN_ASSETS', plugin_dir_url(__FILE__) . 'Assets');
+		define('REVIXREVIEWS_ADMIN_PATH', plugin_dir_path(__FILE__));
+	}
+
+	/**
+	 * Initializes all the admin classes.
+	 *
+	 * @since 1.0.0
+	 */
+	private function init_admin_classes()
+	{
+		$this->assets             = new Assets();
+		$this->reviews            = new Reviews();
+		$this->reviews_meta_box     = new ReviewsMetaBox();
+		$this->settings           = new Settings();
+		$this->core               = new Core();
+	}
 }
--- Admin/Core/Core.php
+++ Admin/Core/Core.php
@@ -1,42 +1,69 @@
 <?php
+/**
+ * Core class for the plugin.
+ *
+ * This class handles the core functionalities of the plugin in the admin area.
+ *
+ * @package RevixReviews
+ * @subpackage Admin
+ * @since 1.0.0
+ */
+
 namespace RevixReviews\Admin\Core;
 
 // Exit if accessed directly.
 if (!defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
+/**
+ * Class Core
+ *
+ * @package RevixReviews\Admin\Core
+ * @since 1.0.0
+ */
 class Core
 {
-    public function __construct()
-    {
-        add_filter('admin_footer_text', [ $this, 'change_admin_footer_text' ]);
-    }
+	/**
+	 * Core constructor.
+	 *
+	 * Initializes the core functionalities.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct()
+	{
+		add_filter('admin_footer_text', [$this, 'change_admin_footer_text']);
+	}
 
-    /**
-     * Adds a custom footer text to the admin pages of the plugin.
-     *
-     * @param string $text The default footer text.
-     * @return string The modified footer text.
-     */
-    public function change_admin_footer_text($text)
-    {
-        // Only show on Revix Reviews pages
-        $screen = get_current_screen();
-        if (
-            $screen && (
-                $screen->id === 'revixreviews_page_revixreviews_settings' ||
-                $screen->post_type === 'revixreviews'
-            )
-        ) {
-            $text = sprintf(
-                __('Please rate <strong>%1$s</strong> %2$s on <a href="%3$s" target="_blank">WordPress.org</a> to help us spread the word.', 'revix-reviews'),
-                'Revix Reviews',
-                str_repeat('<span style="color: #ffb900;">&#9733;</span>', 5),
-                'https://wordpress.org/support/plugin/revix-reviews/reviews/#new-post'
-            );
-        }
+	/**
+	 * Adds a custom footer text to the admin pages of the plugin.
+	 *
+	 * This function checks if the current screen is a Revix Reviews page and, if so,
+	 * replaces the default footer text with a custom one that encourages users to rate the plugin.
+	 *
+	 * @param string $text The default footer text.
+	 * @return string The modified footer text.
+	 * @since 1.0.0
+	 */
+	public function change_admin_footer_text($text)
+	{
+		// Only show on Revix Reviews pages
+		$screen = get_current_screen();
+		if (
+			$screen && (
+				$screen->id === 'revixreviews_page_revixreviews_settings' ||
+				$screen->post_type === 'revixreviews'
+			)
+		) {
+			$text = sprintf(
+				wp_kses_post('Please rate <strong>%1$s</strong> %2$s on <a href="%3$s" target="_blank">WordPress.org</a> to help us spread the word.', 'revix-reviews'),
+				'Revix Reviews',
+				str_repeat('<span style="color: #ffb900;">&#9733;</span>', 5),
+				'https://wordpress.org/support/plugin/revix-reviews/reviews/#new-post'
+			);
+		}
 
-        return $text;
-    }
+		return $text;
+	}
 }
